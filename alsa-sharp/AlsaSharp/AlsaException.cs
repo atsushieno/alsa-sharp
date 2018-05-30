7c0b91c4154b056f411237726ce1b1bdd3a18d50
Atsushi Eno <atsushieno@gmail.com>
2018-05-30 15:08:46 +0900

    AlsaException should give error message for human beings, not just code.

--- alsa-sharp/AlsaSharp/AlsaException.cs
+++ alsa-sharp/AlsaSharp/AlsaException.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace AlsaSharp {
 	public class AlsaException : Exception {
+
+		static string GetErrorMessage (int errorCode)
+		{
+			var ptr = Natives.snd_strerror (errorCode);
+			return ptr != IntPtr.Zero ? Marshal.PtrToStringAnsi (ptr) : null;
+		}
+
 		public AlsaException (int errorCode, Exception innerException = null)
-			: this ($"ALSA exception (error code = {errorCode})", innerException)
+			: this ($"ALSA exception (error code = {errorCode}) : {GetErrorMessage (errorCode)}", innerException)
 		{
 		}
 
